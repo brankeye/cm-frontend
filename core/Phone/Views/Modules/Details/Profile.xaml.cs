eaf4022b8b935b95cb32ae23cc51383c09cb1c82
Brandon Keyes <brankeye@gmail.com>
2016-11-21 04:09:48 -0500

    Playing with modules idea.

---
+++ core/Phone/Views/Modules/Details/Profile.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace cm.frontend.core.Phone.Views.Modules.Details
+{
+    public partial class Profile : StackLayout
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+    }
+}
