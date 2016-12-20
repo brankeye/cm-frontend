2dcfbabc1066f2fa9f7c444210aadf035bc53d77
Brandon Keyes <brankeye@gmail.com>
2016-12-19 19:12:22 -0500

    Added NUnit testing projects to solution.

---
+++ testing/client/Droid/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace cm.frontend.testing.client.Droid
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
---
+++ testing/core/Phone/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace cm.frontend.testing.core.Phone
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
